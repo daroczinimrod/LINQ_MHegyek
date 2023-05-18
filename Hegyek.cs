74254e835f725d4216e83bff19270da552c8a54f
rodi <72067090+daroczinimrod@users.noreply.github.com>
2023-05-18 14:20:22 +0200

    Add files via upload

---
+++ Hegyek.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_MHegyek
+{
+    internal class Hegyek
+    {
+        string hegyNev;
+        string hegyseg;
+        int magassag;
+
+        public Hegyek(string hegynev, string hegyseg, int magassag)
+        {
+            this.hegyNev = hegynev;
+            this.hegyseg = hegyseg;
+            this.magassag = magassag;
+        }
+
+        public string HegyNev { get => hegyNev; }
+        public string Hegyseg { get => hegyseg; }
+        public int Magassag { get => magassag; }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using LINQ_MHegyek;
+
+
+List<Hegyek> Hegyek = new List<Hegyek>();
+
+
+StreamReader olvas = new StreamReader("hegyekMo.txt");
+olvas.ReadLine();
+while (!olvas.EndOfStream)
+{
+    string[] adat = olvas.ReadLine().Split(';');
+    Hegyek.Add(new Hegyek(adat[0], adat[1], int.Parse(adat[2])));
+}
+olvas.Close();
+
+Console.WriteLine($"3.Feladat : Hegycsucsok száma:  { Hegyek.Count()} db");
+Console.WriteLine($"4.Feladat : Hegycsucsok atlagos magassaga: {Math.Round(Hegyek.Average(x => x.Magassag), 2)} m");
